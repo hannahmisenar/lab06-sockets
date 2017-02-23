103b629255f302bb3db79468903f56546b216285
hmisenar <hannahamisenar@gmail.com>
2017-02-22 22:53:19 -0800

    Lab Submission

--- src/client.ts
+++ src/client.ts
@@ -9,11 +9,13 @@ const net = require('net'),
     
 
 client.on('data', function(data) { //when we get data
-    
+       console.log("We received your data: "+data + '\n');
 });
 
 client.on('close', function() { //when connection closed
-    
+    console.log('We are disconnecting the server');
+    console.log('The client is being closed');
+    exit(0);
 });
 
 
@@ -22,7 +24,17 @@ var PORT = 3000;
 
 //connect to the server
 client.connect(PORT, HOST, function() {
+    console.log('connected to ' + HOST +  ":" + PORT);
+    
+    io.setPrompt('input>');
 
+    io.prompt();
+
+    io.onclose('close', function() {
+        client.end();
+        console.log('client disconnecting');
+        exit(0);
+    });
 });
 
 
